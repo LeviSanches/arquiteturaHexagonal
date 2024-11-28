851f19f6113b1c492d698aeacb3a6c990e3e874c
Levi Sanches <levisanches@capgemini.com>
2024-11-28 11:52:06 -0300

    feat: finalizando o controller da inserção de dados

--- src/main/java/com/levi/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/levi/hexagonal/adapters/in/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package com.levi.hexagonal.adapters.in.controller;
 
+import com.levi.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import com.levi.hexagonal.adapters.in.controller.request.CustomerRequest;
+import com.levi.hexagonal.application.ports.in.InsertCustomerInputPort;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
 
+    @Autowired
+    private InsertCustomerInputPort insertCustomerInputPort;
+
+    @Autowired
+    private CustomerMapper customerMapper;
+
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest customerRequest) {
+        var customer = customerMapper.toCustomer(customerRequest);
+        insertCustomerInputPort.insert(customer, customerRequest.getZipCode());
 
+        return ResponseEntity.ok().build();
     }
 }
